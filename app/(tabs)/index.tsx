4e0a78ed0b2d65cdd42e55b0e34c3db55148a4ce
dr-msr <me@drmsr.dev>
2023-07-12 14:32:15 +0800

    themecheck

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,16 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import Changelog from '../../components/changelog';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Changelog Update</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={{width:'100%', paddingLeft:10, paddingRight:10}} lightColor='#ffffff'>
+      <Changelog/></View>
+      <Text>Test</Text>
     </View>
   );
 }
@@ -22,6 +25,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop:10,
   },
   separator: {
     marginVertical: 30,
